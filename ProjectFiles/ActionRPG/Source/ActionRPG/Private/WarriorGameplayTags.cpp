758e037065962b56f01121c799974a70c5dd33bc
Sarah Watts <135098414+wxttsy@users.noreply.github.com>
2024-10-01 14:40:40 +1000

    Added Native Gameplay Tags
    -Created new C++ class called WarriorGameplayTags for defining the input tags.
    -Adjusted Camera position in BP_HeroCharacter.

---
+++ ProjectFiles/ActionRPG/Source/ActionRPG/Private/WarriorGameplayTags.cpp
@@ -0,0 +1,10 @@
+// Sylbie All Rights Reserved
+
+
+#include "WarriorGameplayTags.h"
+
+namespace WarriorGameplayTags {
+	/** Input Tags **/
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Move,"InputTag.Move")
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Look, "InputTag.Look")
+}
---
+++ ProjectFiles/ActionRPG/Source/ActionRPG/Public/WarriorGameplayTags.h
@@ -0,0 +1,11 @@
+// Sylbie All Rights Reserved
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace WarriorGameplayTags {
+	/** Input Tags **/
+	ACTIONRPG_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Move)
+	ACTIONRPG_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Look)
+}
